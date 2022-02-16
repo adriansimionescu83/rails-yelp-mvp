5472a7ffb56f2af37c87b54b33bdd415eb16726c
Adrian Simionescu <simionescuadrian@yahoo.com>
2022-02-16 15:49:48 +0400

    Solution

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+  require 'faker'
+#This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+categories = %w(chinese italian japanese french belgian)
+
+10.times do
+  Restaurant.create(name: Faker::Restaurant.name, address: Faker::Address.street_address, category: categories.sample, phone_number: Faker::PhoneNumber.phone_number)
+end
